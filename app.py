1836525e31084b90c489c15e456a3266d94a35b4
Thammak <133721351+Thammak@users.noreply.github.com>
2023-07-15 01:30:37 +0530

    created a database and modified login and register logic
    created a database using sql alchemy and modified the code for login and register code

--- app.py
+++ app.py
@@ -2,19 +2,64 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'            #created a database which stores and registers our id and password 
+db = SQLAlchemy(app)
+app.secret_key = 'your_secret_key'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
-      return render_template('register.html')
+def register():                           # registeration logic
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            error = 'Username already exists. Please choose a different username.'
+            return render_template('register.html', error=error)
+
+        # Insert the new user into the database
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-      return render_template('login.html')
+def login():                                        # registeration logic
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Retrieve the user from the database
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            # Successful login
+            # Store the username in the session
+            session['username'] = username
+
+            return redirect(url_for('dashboard'))
+
+        error = 'Invalid username or password.'
+        return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
 
 
 @app.route('/dashboard')
