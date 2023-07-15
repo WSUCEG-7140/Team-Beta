d5bade3369f4087c0788941f59bd187f15657679
Mohanakavyac <133721594+Mohanakavyac@users.noreply.github.com>
2023-07-15 18:12:18 -0400

    Add files via upload

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+def test_index():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"<title>IQ Test</title>" in response.data
+
+
