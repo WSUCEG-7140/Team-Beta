f10d46b1666f6fa5af75db27aaebc0ca99a13f56
Mohanakavyac <133721594+Mohanakavyac@users.noreply.github.com>
2023-07-15 18:17:03 -0400

    Added test_register

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,7 @@ def test_register_page():
     response = app.test_client().get('/register')
     assert response.status_code == 200
 
+def test_register():
+    response = app.test_client().post('/register', data={"username": "New_User", "password": "NewPassword"})
+    assert response.status_code == 200
+
