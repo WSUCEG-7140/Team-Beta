c4c6b69bd9b33a2ba8c55c688fb6b4a3783221e9
Mohanakavyac <133721594+Mohanakavyac@users.noreply.github.com>
2023-07-15 18:20:10 -0400

    Added test_quizpage

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,3 +24,7 @@ def test_login():
 def test_dashboard():
     response = app.test_client().get('/dashboard')
     assert b"<h1>Welcome to the Dashboard</h1>" in response.data
+
+def test_quiz_page():
+    response = app.test_client().get('/quiz')
+    assert b"<h1>Quiz</h1>" in response.data
