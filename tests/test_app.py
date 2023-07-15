d3176caf5dd339e98d2d80905df41887262e14bc
Mohanakavyac <133721594+Mohanakavyac@users.noreply.github.com>
2023-07-15 18:15:17 -0400

    Added test case for register page

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,8 @@ def test_index():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert b"<title>IQ Test</title>" in response.data
-
+    
+def test_register_page():
+    response = app.test_client().get('/register')
+    assert response.status_code == 200
 
