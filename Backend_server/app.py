738eaac41fda269750c25249aab385ae8cef4d7f
Thammak <133721351+Thammak@users.noreply.github.com>
2023-07-27 13:34:04 -0400

    Updated code in app.py

--- Backend_server/app.py
+++ Backend_server/app.py
@@ -1,7 +1,7 @@
 """@ref R104_0"""
 
-# Importing required modules from Flask and other libraries
 from flask import Flask, render_template, request, redirect, url_for, session
+
 from flask_sqlalchemy import SQLAlchemy
 import csv
 import random
@@ -9,26 +9,21 @@ import vectorize as vectorizer
 import pickle
 import json
 
-#Create a Flask app
 app = Flask(__name__)
 
-# Configure the SQLite database for storing user data
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' #created a database which stores and registers our id and password 
 db = SQLAlchemy(app)
 app.secret_key = 'your_secret_key'
 
-# Define the User model for the database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
-# Route: Homepage (Index page)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route: User registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -50,7 +45,6 @@ def register():
 
     return render_template('register.html')
 
-# Route: User login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -72,7 +66,7 @@ def login():
 
     return render_template('login.html')
 
-# Route: Dashboard page (user's personalized page)
+
 @app.route('/dashboard')
 def dashboard():
     # Retrieve the username from the session
@@ -236,4 +230,3 @@ def result():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
