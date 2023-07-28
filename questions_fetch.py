0b61db8e9119c625450ea78a00622e60080dd7ad
PoojithaKamana <133623845+PoojithaKamana@users.noreply.github.com>
2023-07-28 16:41:26 -0400

    Updated comments in questions_fetch.py

--- questions_fetch.py
+++ questions_fetch.py
@@ -6,6 +6,15 @@ import csv       # For working with CSV files
 
 # Function to fetch IQ questions from open source resources and store them in a CSV file
 def fetch_iq_questions():
+    """
+   @brief: This function fetches IQ questions from a list of URLs of open-source resources and stores them in a CSV file named "iq_questions.csv."
+   @pre: The function expects the 'requests' library to be imported for making HTTP requests.The 'csv' module must be imported for writing the 
+   IQ questions to the CSV file.The 'iq_questions.csv' file should not exist in the current working directory to avoid overwriting any existing data.
+   @post: The 'iq_questions.csv' file will be created in the current working directory with the extracted IQ questions.
+   Each IQ question will be written as a separate row in the CSV file.If the HTTP GET request to any resource fails, an error message will be printed to the console.
+   @return: The function fetches IQ questions from URLs and writes them to the "iq_questions.csv" file. It prints an error message if any request fails but continues fetching questions    
+   from other resources. If all requests are successful, the function completes its execution without any explicit return value.
+   """
     # List of URLs of open source resources that contain IQ questions
     resources = [
         "https://www.iqtest.com/questions",
